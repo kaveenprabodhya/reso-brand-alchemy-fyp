545ecfe3444c3ecc9803a36b6b89a2c667f368e2
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2024-03-08 00:02:57 +0530

    add emotion colors

--- src/components/Home/artistpreferences.js
+++ src/components/Home/artistpreferences.js
@@ -16,6 +16,10 @@ const ArtistPreferences = ({
   fileInputRef,
   deleted,
 }) => {
+  const emotions = ["Sad", "Sorrow", "Distress"];
+
+  const colors = ["red", "green", "blue"];
+
   return (
     <>
       <div className="container">
@@ -79,17 +83,69 @@ const ArtistPreferences = ({
           <h6 className="me-auto mt-3 mb-3 text-white">Your prefernces</h6>
           <div
             className="bg-white mt-2"
-            style={{ width: "100%", height: "280px", overflowY: "auto" }}
+            style={{ width: "100%", height: "40%", overflowY: "auto" }}
           >
-            <div className="bg-secondary w-100 h-50 my-2 opacity-50 text-white">
+            <div className="bg-secondary w-100 my-2 py-1 opacity-50 text-white">
               <div className="ps-2 pt-2">Emotions Captured</div>
               <div className={classes.horizontalSeparator}></div>
+              <div
+                className="my-2 mx-3"
+                style={{
+                  display: "grid",
+                  gridTemplateRows: "1fr",
+                  height: "100%",
+                }}
+              >
+                {emotions.length <= 0 && (
+                  <div className="d-flex border justify-content-center align-items-center">
+                    No emotions captured.
+                  </div>
+                )}
+                <ul className="list-group list-group-flush rounded">
+                  {emotions.map((emotion, index) => (
+                    <li className="list-group-item" key={index}>
+                      {emotion}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            <div className="bg-secondary w-100 h-50 my-2 opacity-50 text-white">
+            <div
+              className={`bg-secondary w-100 my-2 py-1 opacity-50 text-white`}
+            >
               <div className="ps-2 pt-2">Recommended Colors</div>
               <div className={classes.horizontalSeparator}></div>
+              <div
+                className="px-2"
+                style={{
+                  display: "grid",
+                  gridTemplateRows: "1fr",
+                  height: "100%",
+                }}
+              >
+                {colors.length <= 0 && (
+                  <div className="d-flex border justify-content-center align-items-center">
+                    No color suggestions.
+                  </div>
+                )}
+                {colors.map((color, index) => (
+                  <div
+                    className="bg-white d-flex align-items-center justify-content-center"
+                    style={{ height: "75px", width: "75px" }}
+                    key={index}
+                  >
+                    <div
+                      style={{
+                        backgroundColor: color,
+                        width: "60px",
+                        height: "60px",
+                      }}
+                    ></div>
+                  </div>
+                ))}
+              </div>
             </div>
-            <div className="bg-secondary w-100 h-50 my-2 opacity-50 text-white">
+            <div className={`bg-secondary w-100 my-2 opacity-50 text-white`}>
               <div className="ps-2 pt-2">Additional Colors</div>
               <div className={classes.horizontalSeparator}></div>
             </div>
